b55f43400e2aa05e05e5d6c874b94e2289558237
bsbanotto <ben.sbanotto@gmail.com>
2023-01-09 12:42:05 -0600

    Commit task 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This file will return the shape fof an array
+"""
+
+
+def np_shape(matrix):
+    """
+    This function will use numpy to return the shape of an array
+    """
+    return matrix.shape
