33500a97a3ffd4e62ab5f97842cb0d743f3975b1
bsbanotto <ben.sbanotto@gmail.com>
2023-01-09 12:25:47 -0600

    Fixxed pycodestyle

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -8,13 +8,13 @@ def mat_mul(mat1, mat2):
     """
     Dot product implementation
     """
-    return_matrix = [[0 for x in range(len(mat2[0]))] for y in range(len(mat1))]
+    return_mat = [[0 for x in range(len(mat2[0]))] for y in range(len(mat1))]
     if(len(mat1[0]) != len(mat2)):
         return None
 
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
-                return_matrix[i][j] += mat1[i][k] * mat2[k][j]
+                return_mat[i][j] += mat1[i][k] * mat2[k][j]
 
-    return return_matrix
+    return return_mat
