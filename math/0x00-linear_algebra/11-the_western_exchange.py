52af1549aee04e5e56116a8b48f230bc72f679d5
bsbanotto <ben.sbanotto@gmail.com>
2023-01-09 12:46:13 -0600

    First commit task 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This file uses numpy to transpose a matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+    This function does what the file does
+    """
+    return matrix.transpose()
