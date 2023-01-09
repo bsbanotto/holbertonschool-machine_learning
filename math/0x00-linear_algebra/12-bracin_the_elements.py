deeb855de778c4852d62e0733b16f11156a74b5b
bsbanotto <ben.sbanotto@gmail.com>
2023-01-09 12:57:11 -0600

    Commit for task 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This file will perform element-wise math operations
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Rerurns mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat 2
+    """
+    return(mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
+    
