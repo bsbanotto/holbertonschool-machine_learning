db67c3904b9254f5bb60e8e570e7d0828c00cfe1
bsbanotto <ben.sbanotto@gmail.com>
2023-01-09 11:18:08 -0600

    Commit task 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This file concatenates two matrices
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    This funciton concatenates two matrices
+    Must return a new list
+    """
+    new_array = []
+    new_array = arr1 + arr2
+    return new_array
