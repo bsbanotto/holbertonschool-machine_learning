7d9342b00a0af2331252b76c161980b9122a4adb
bsbanotto <ben.sbanotto@gmail.com>
2023-01-09 13:10:27 -0600

    Commit task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This file performs matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    return_array = np.dot(mat1, mat2)
+    return return_array
