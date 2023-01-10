9d4791f065068889d427667cef48c47b34d49c9a
bsbanotto <ben.sbanotto@gmail.com>
2023-01-10 09:56:30 -0600

    Added bar width requirements

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -10,19 +10,17 @@ apples = fruit[0]
 bananas = fruit[1]
 oranges = fruit[2]
 peaches = fruit[3]
-print("apples = ", apples)
-print("bananas = ", bananas)
-print("oranges = ", oranges)
-print("peaches = ", peaches)
 labels = ['Farrah', 'Fred', 'Felicia']
 plt.ylim(0, 80)
+plt.yticks(range(0, 81, 10))
 plt.title("Number of Fruit per Person")
 plt.ylabel("Quantity of Fruit")
-plt.bar(labels, fruit[0][:], color='red', label='apples')
-plt.bar(labels, fruit[1][:], color='yellow', bottom=apples, label='bananas')
+plt.bar(labels, fruit[0][:], color='red', label='apples', width=.5)
+plt.bar(labels, fruit[1][:], color='yellow', bottom=apples, label='bananas',
+        width=.5)
 plt.bar(labels, fruit[2][:], color='#FF8000', bottom=apples + bananas,
-        label='oranges')
+        label='oranges', width=.5)
 plt.bar(labels, fruit[3][:], color='#FFE5B4',
-        bottom=apples + bananas + oranges, label='peaches')
+        bottom=apples + bananas + oranges, label='peaches', width=.5)
 plt.legend()
 plt.show()
