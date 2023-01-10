603d79aeaf59e874b38719986549a26c4fc97ffa
bsbanotto <ben.sbanotto@gmail.com>
2023-01-10 09:14:48 -0600

    Commit for task 5

--- math/0x01-plotting/5-all_in_one.py
+++ math/0x01-plotting/5-all_in_one.py
@@ -37,15 +37,41 @@ ax00.set_xlim([0, 10])
 
 """Top Right Scatter Plot"""
 ax01 = fig.add_subplot(spec[0, 1])
+ax01.scatter(x1, y1, color='magenta', marker='.')
+ax01.set_xlabel('Height (in)', fontsize='x-small')
+ax01.set_ylabel('Weight (lbs)', fontsize='x-small')
+ax01.set_title("Men's Height vs Weight", fontsize='x-small')
 
 """Middle Left Semilog Plot"""
 ax10 = fig.add_subplot(spec[1, 0])
+ax10.semilogy(x2, y2)
+ax10.set_xlim(0, 28650)
+ax10.set_xlabel("Time (years)", fontsize='x-small')
+ax10.set_ylabel("Fraction Remaining", fontsize='x-small')
+ax10.set_title("Exponential Decay of C-14", fontsize='x-small')
 
-"""Middle Right Semilog Plot"""
+
+"""Middle Right Exponential Decay"""
 ax11 = fig.add_subplot(spec[1, 1])
+ax11.plot(x3, y31, c="red", linestyle="--", label="C-14")
+ax11.plot(x3, y32, c="green", label="Ra-226")
+ax11.set_xlim(0, 20000)
+ax11.set_ylim(0, 1)
+ax11.set_title("Exponential Decay of Radioactive Elements", fontsize='x-small')
+ax11.set_xlabel("Time (years)", fontsize='x-small')
+ax11.set_ylabel("Fraction Remaining", fontsize='x-small')
+ax11.legend()
 
 """Bottom Histogram"""
 ax20 = fig.add_subplot(spec[2, :])
+ax20.hist(student_grades, facecolor="#348CC4", edgecolor="black",
+         bins=range(0, 101, 10), linewidth=0.75)
+ax20.set_xlim(0, 100)
+ax20.set_xticks(ticks=range(0, 101, 10))
+ax20.set_ylim(0, 30)
+ax20.set_title("Project A", fontsize='x-small')
+ax20.set_ylabel("Number of Students", fontsize='x-small')
+ax20.set_xlabel("Grades", fontsize='x-small')
 
 """Plot Specs"""
 fig.suptitle("All in One")
