7bf19a9193a1fed1aea28cead91d4e3bc431bd41
bsbanotto <ben.sbanotto@gmail.com>
2023-01-17 13:22:19 -0600

    Added some guards for poly

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -9,7 +9,9 @@ def poly_derivative(poly):
     poly is a list of coefficients representing a polynomial
     return the derivative
     """
-    if(type(poly) == list):
+    if len(poly) == 1:
+        return 0
+    if(type(poly) == list) or len(poly) > 0:
         return_list = []
         for i in range(1, len(poly)):
             return_list.append(poly[i] * i)
