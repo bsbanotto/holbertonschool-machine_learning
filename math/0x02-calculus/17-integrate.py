7ba055acf4aab757c1d40e500f57126479ee9eda
bsbanotto <ben.sbanotto@gmail.com>
2023-01-17 14:24:35 -0600

    Commit for task 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This file determines the coefficients of a polynomial integral
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+    Given a polynomial, determine the coefficients of the integral
+    The integration constant is given as C
+    """
+    return_poly = []
+    return_poly.append(C)
+    for i in range(0, len(poly)):
+        append_value = poly[i] / (i + 1)
+        if (float(append_value) == int(append_value)):
+            append_value = int(append_value)
+        return_poly.append(append_value)
+    return (return_poly)
