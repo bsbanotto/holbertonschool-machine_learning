0bc899009ea32ab1163a3a2d745176067e0ab567
bsbanotto <ben.sbanotto@gmail.com>
2023-01-23 11:00:23 -0600

    Commit for task 8

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -43,3 +43,13 @@ class Normal:
         Calculates the x-value of a given z-score
         """
         return ((z * self.stddev) + self.mean)
+
+    def pdf(self, x):
+        """
+        Calculates the value of the probability density function (pdf) for x
+        see this link for the formula 
+        https://en.wikipedia.org/wiki/Normal_distribution
+        """
+        val1 = 1 / (self.stddev * ((2 * pi) ** .5))
+        val2 = -.5 * ((x - self.mean) / self.stddev) ** 2
+        return (val1 * e ** val2)
