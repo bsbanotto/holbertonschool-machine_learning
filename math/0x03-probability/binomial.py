496a1f6d4b49bf2b4dfb895018100d22093accf1
bsbanotto <ben.sbanotto@gmail.com>
2023-01-23 12:15:47 -0600

    Checking to make sure I covered all of the required tests

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+This module defines the binomial distribution
+"""
+pi = 3.1415926536
+e = 2.7182818285
+
+
+class Binomial:
+    """
+    This class is the Binomail distribution
+    """
+    def __init__(self, data=None, n=1, p=0.5):
+        """
+        Initialize a Binomial distribution
+        """
+        self.n = int(n)
+        self.p = float(p)
+        if data is None:
+            if n <=0:
+                raise ValueError("n must be a positive value")
+            if p <= 0 and p >= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+        if type(data) is not list:
+            raise TypeError("data must be a list")
+        if len(data) <= 1:
+            raise ValueError("data must contain multiple values")
+        
