49c886440189bd2105b0efa0d6b9fa4dc9be3c95
bsbanotto <ben.sbanotto@gmail.com>
2023-02-03 13:00:00 -0600

    Committing tasks 3 and 4

---
+++ supervised_learning/0x01-classification/4-neuron.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+Neuron module
+"""
+import numpy as np
+
+
+class Neuron():
+    """
+    Constructs a Neuron
+    """
+    def __init__(self, nx):
+        """
+        Initialize Neuron class, privatizing W, b, A
+        """
+        self.__b = 0
+        self.__A = 0
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        elif(nx < 1):
+            raise ValueError("nx must be a positive integer")
+        else:
+            self.__W = np.random.randn(1, nx)
+
+    @property
+    def A(self):
+        """
+        Gets the Activation value
+        """
+        return self.__A
+
+    @property
+    def W(self):
+        """
+        Gets the Weight value
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        Gets the bias value
+        """
+        return self.__b
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propogation of the neuron
+        Matrix Multiply Weights and given numpy array, add bias
+        Pass this to the sigmoid function
+        """
+        x = np.matmul(self.__W, X) + self.__b
+        sigmoid = (1 / (1 + np.exp(-x)))
+        self.__A = sigmoid
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of a model using logistic regression
+        Loss applies to a specific training example
+        Cost is the sum of the Losses
+        """
+        one = 1.0000001
+        loss = Y * np.log(A) + (1 - Y) * np.log(one - A)
+        cost = -(1 / A.shape[1]) * np.sum(loss)
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neurons predictions
+        """
+        return (np.rint(self.forward_prop(X)).astype(int),
+                self.cost(Y, self.__A))
