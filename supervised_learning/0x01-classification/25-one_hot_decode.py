ebf383345d8dcf24a905bcb8cfa319e48923dec5
bsbanotto <ben.sbanotto@gmail.com>
2023-02-06 20:40:45 -0600

    Forgot I could not use loops

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -9,15 +9,7 @@ def one_hot_decode(one_hot):
     """
     Function to decode a one-hot matrix into a vector of labels
     """
-    onehot_decode = []
     try:
-        for row in one_hot.T:
-            for i in range(0, len(row)):
-                if row[i] == 0:
-                    i += 1
-                if row[i] == 1:
-                    break
-            onehot_decode.append(i)
-        return(np.asarray(onehot_decode))
+        return (one_hot.argmax(0))
     except Exception:
         return None
