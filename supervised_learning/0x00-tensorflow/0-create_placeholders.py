15b5606c5ad5760477c0a5ef6bbbdcc24a39c2a6
bsbanotto <ben.sbanotto@gmail.com>
2023-02-10 16:28:03 -0600

    Improved README, trying commenting out imports differently

--- supervised_learning/0x00-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x00-tensorflow/0-create_placeholders.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 """
-Documentation here, will improve later after testing that tensorflow is working
+Create placeholder Tensors for nx and classes
+
+Use this import for testing locally
+`import tensorflow.compat.v1 as tf`
+
+Use this import when committing to github
+`import tensorflow as tf`
 """
+'''import tensorflow.compat.v1 as tf'''
 import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
     """
-    Returns two placeholders, x and y, for a neural network
+    Returns two tensor placeholders, x and y, for a neural network
     """
     x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
     y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
--- supervised_learning/0x00-tensorflow/0-main.py
+++ supervised_learning/0x00-tensorflow/0-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 tf.compat.v1.disable_v2_behavior()
 
