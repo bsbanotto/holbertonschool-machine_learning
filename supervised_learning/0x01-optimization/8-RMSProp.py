54ed57c467e0c58727db40a2f99343fdcd718886
bsbanotto <ben.sbanotto@gmail.com>
2023-02-20 09:46:07 -0600

    Commit for task 8

---
+++ supervised_learning/0x01-optimization/8-RMSProp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Creates the training operation for a neural network using RMSProp in tensorflow
+"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    Implementation of RMSProp algo using tensorflow
+    loss: Loss of the network
+    alpha: Learning rate
+    beta2: RMSProp weight
+    epsilon: Small number to avoid divide by zero error
+    Returns the RMSProp optimization operation
+    """
+    return tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                     momentum=beta2,
+                                     epsilon=epsilon).minimize(loss)
