e0d3a822ba3daa76462fc820f02eac8c4544aefe
bsbanotto <ben.sbanotto@gmail.com>
2023-02-24 13:25:57 -0600

    Added imports to task 4

--- supervised_learning/0x00-error_analysis/4-f1_score.py
+++ supervised_learning/0x00-error_analysis/4-f1_score.py
@@ -3,6 +3,8 @@
 Calculate the F1 score of a confusion matrix
 """
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
@@ -13,10 +15,6 @@ def f1_score(confusion):
     Returns a numpy.ndarray of shape (classes, ) containing the F1 score of
         each class
     """
-    # prec = precision(confusion)
-    # sens = sensitivity(confusion)
-    # F1 = prec + sens
-    # return F1 / 2
 
     true_positives = np.diag(confusion)
     col_sum = np.sum(confusion, axis=0)
