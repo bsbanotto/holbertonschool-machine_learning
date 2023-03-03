93520705b6f39ebbe75ace6467d22764165543ca
bsbanotto <ben.sbanotto@gmail.com>
2023-03-03 10:30:04 -0600

    Refactored to try to appease checker

--- supervised_learning/0x02-keras/0-sequential.py
+++ supervised_learning/0x02-keras/0-sequential.py
@@ -19,14 +19,17 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     model = K.Sequential()
     # Create our input layer
-    model.add(K.layers.InputLayer(input_shape=(nx,)))
+    model.add(K.layers.Dense(units=layers[0],
+                             activation = activations[0],
+                             kernel_regularizer=K.regularizers.L2(lambtha),
+                             input_shape=(nx,)))
     # Create all of our hidden layers
-    for layer in range(len(layers)):
+    for layer in range(1, len(layers)):
+        if layer <= len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(units=layers[layer],
                                  activation=activations[layer],
                                  kernel_regularizer=K.regularizers.L2(lambtha)
                                  ))
         # Handle dropout on hidden layers
-        if layer < len(layers) - 1:
-            model.add(K.layers.Dropout(1 - keep_prob))
     return model
