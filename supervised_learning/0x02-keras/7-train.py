c74e3cd7e201bbd5d5377c774f3887acfec83044
bsbanotto <ben.sbanotto@gmail.com>
2023-03-06 11:52:04 -0600

    That name thing really sent me down a rabbit hole

--- supervised_learning/0x02-keras/7-train.py
+++ supervised_learning/0x02-keras/7-train.py
@@ -7,9 +7,9 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
-                  validation_data=None, early_stopping=False, patience=0,
-                  learning_rate_decay=False, alpha=0.1, decay_rate=1,
-                  verbose=True, shuffle=False):
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                verbose=True, shuffle=False):
     """
     learning_rate_decay: boolean that indicates whether or not learning rate
     decay should be used
@@ -54,12 +54,12 @@ def train_model(network, data, labels, batch_size, epochs,
                 return alpha / (1 + decay_rate * (epoch))
             learningratedecay = K.callbacks.LearningRateScheduler(scheduler,
                                                                   verbose)
-    return network.fit(x=data,
-                       y=labels,
-                       batch_size=batch_size,
-                       epochs=epochs,
-                       verbose=verbose,
-                       shuffle=shuffle,
-                       validation_data=validation_data,
-                       callbacks=[earlystopping, learningratedecay]
-                       )
+        return network.fit(x=data,
+                           y=labels,
+                           batch_size=batch_size,
+                           epochs=epochs,
+                           verbose=verbose,
+                           shuffle=shuffle,
+                           validation_data=validation_data,
+                           callbacks=[earlystopping, learningratedecay]
+                           )
