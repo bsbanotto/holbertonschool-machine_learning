a633d7f7173d32998765c18417e85d91d9566b18
bsbanotto <ben.sbanotto@gmail.com>
2023-03-06 15:05:35 -0600

    Commit task 12

---
+++ supervised_learning/0x02-keras/12-test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Write a function that tests a neural network
+"""
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    network: network model to test
+    data: input data to test the model with
+    labels: correct one-hot labels of data
+    verbose: boolean that determines if output should be printed during
+    the testing process
+    Returns the loss and accuracy of the model with the testing data
+    respectively
+    """
+    return network.evaluate(x=data, y=labels, verbose=verbose)
