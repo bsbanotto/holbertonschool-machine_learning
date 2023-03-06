688749aa19d43245e68dbc6cc981a8f17629021a
bsbanotto <ben.sbanotto@gmail.com>
2023-03-06 10:19:02 -0600

    Commit for task 5

---
+++ supervised_learning/0x02-keras/5-train.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Based on 4. Train, update the function def train_model to also analyze
+validation data
+"""
+import tensorflow.keras as K
+
+
+def train_model_5(network, data, labels, batch_size, epochs,
+                  validation_data=None, verbose=True, shuffle=False):
+    """
+    validation_data: the data to validate the model with, if not `None`
+    network: model to train
+    data: numpy.ndarray of shape (m, nx) containing the input data
+        m: number of data points
+        nx: number of features
+    labels: one-hot numpy.ndarray of shape (m, classes) with the data labels
+        m: number of data points
+        classes: number of classes
+    batch_size: size of the batch used for mini-batch gradient descent
+    epochs: number of passes through data for mini-batch gradient descent
+    verbose: boolean that determines if the output should be printed during
+        training
+    shuffle: boolean that determines whether to shuffle the batches every
+        epoch. Normally, it is a good idea to shuffle, but for reproducibility
+        we have chosen to set the default to False
+    Returns: the History object generated after training the model
+    """
+    return network.fit(x=data,
+                       y=labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data
+                       )
