de5d0c4940d7694004aee1124f7582ab86e526c1
bsbanotto <ben.sbanotto@gmail.com>
2023-03-07 08:52:06 -0600

    Cleaned up a lot of my hang over logic from forcing padding side to hopefully help

--- math/0x03-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x03-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -20,22 +20,23 @@ def convolve_grayscale_same(images, kernel):
     Only allowed to use two for loops, no loops of other kind
     Returns a numpy.ndarray containing the convolved images
     """
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    
     kh = kernel.shape[0]
     kw = kernel.shape[1]
 
     pad_top_bottom = kh // 2
     pad_left_right = kw // 2
 
+    csm = np.zeros((m, h, w))
+
     images = np.pad(images, ((0, 0), (pad_top_bottom, pad_top_bottom),
                              (pad_left_right, pad_left_right)))
-    m = images.shape[0]
-    h = images.shape[1]
-    w = images.shape[2]
 
-    csm = np.zeros((m, h - pad_top_bottom * 2, w - pad_left_right * 2))
-
-    for x in range(1, (h - 2)):
-        for y in range(1, (w - 2)):
+    for x in range(h):
+        for y in range(w):
             hadamard_prod = np.multiply(images[:, x:x + kh, y:y + kw], kernel)
             csm[:, x, y] = np.sum(hadamard_prod, axis=(1, 2))
     return csm
