133748b226264a8edac24892ff127d4474ec6819
bsbanotto <ben.sbanotto@gmail.com>
2023-03-20 14:38:34 -0500

    Commit for task 3

---
+++ supervised_learning/0x01-deep_cnns/3-projection_block.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Write a function that builds an projection block as described in
+    `Deep Residual Learning for Image Recognition(2015)`
+"""
+import tensorflow.keras as K
+
+
+def projection_block(A_prev, filters, s=2):
+    """
+    A_prev: output from the previous layer
+    filters: tuple or list containing F11, F3, F12 respectively
+        F11: number of filters in the first 1x1 convolution
+        F3: number of filters in the 3x3 convolution
+        F12: numnber of filters in the second 1x1 convolution
+    s: stride for the first convolution in both the main path and the shortcut
+        connection
+    All convolutions inside the block should be followed by batch normalization
+        along the channels axis and a rectified linear activation (ReLU)
+    All weights should use he normal initialization
+    Returns the activated output of the identity block
+    """
+    F11 = filters[0]
+    F3 = filters[1]
+    F12 = filters[2]
+
+    init = K.initializers.he_normal()
+
+    conv1x1_0 = K.layers.Conv2D(filters=F11,
+                                kernel_size=(1, 1),
+                                strides=s,
+                                padding='same',
+                                kernel_initializer=init
+                                )(A_prev)
+
+    batch_norm_0 = K.layers.BatchNormalization(axis=3)(conv1x1_0)
+
+    relu_0 = K.layers.ReLU()(batch_norm_0)
+
+    conv3x3_1 = K.layers.Conv2D(filters=F3,
+                                kernel_size=(3, 3),
+                                strides=s,
+                                padding='same',
+                                kernel_initializer=init)(relu_0)
+
+    batch_norm_1 = K.layers.BatchNormalization(axis=3)(conv3x3_1)
+
+    relu_1 = K.layers.ReLU()(batch_norm_1)
+
+    conv1x1_2 = K.layers.Conv2D(filters=F12,
+                                kernel_size=(1, 1),
+                                strides=s,
+                                padding='same',
+                                kernel_initializer=init)(relu_1)
+
+    batch_norm_2 = K.layers.BatchNormalization(axis=3)(conv1x1_2)
+
+    conv1x1_shortcut = K.layers.Conv2D(filters=F12,
+                                       kernel_size=(1, 1),
+                                       strides=s,
+                                       padding='same',
+                                       kernel_initializer=init)(relu_1)
+
+    batch_norm_shortcut = K.layers.BatchNormalization(axis=3)(conv1x1_shortcut)
+
+    add_layer = K.layers.Add()([batch_norm_2, batch_norm_shortcut])
+
+    relu_output = K.layers.ReLU()(add_layer)
+
+    return relu_output
