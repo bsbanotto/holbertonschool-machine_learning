98d7565b351d7dccbd2978879dc3c71901925a45
bsbanotto <ben.sbanotto@gmail.com>
2023-05-09 14:07:59 -0500

    Added check for symmetry for checker?

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -5,6 +5,21 @@ Function that calculates the definiteness of a matrix
 import numpy as np
 
 
+def check_symmetric(matrix):
+    """
+    Checks whether a matrix is symmertic
+    """
+    rows = len(matrix)
+    if rows > 0:
+        cols = len(matrix[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if (matrix[row][col] != matrix[col][row]):
+                return False
+    return True
+
+
 def definiteness(matrix):
     """
     matrix: numpy.ndarray shape (n, n) whose definiteness should be calculated
@@ -21,6 +36,8 @@ def definiteness(matrix):
     """
     if type(matrix) is not np.ndarray:
         raise TypeError("matrix must be a numpy.ndarray")
+    if check_symmetric(matrix) is False:
+        return None
     try:
         eigvals = np.linalg.eigvals(matrix)
     except np.linalg.LinAlgError:
