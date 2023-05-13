357abbea088156448cce8c291490ed41fbcf17b2
bsbanotto <ben.sbanotto@gmail.com>
2023-05-13 09:40:51 -0500

    Still tweaking all of the type/value errors

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -30,14 +30,14 @@ def likelihood(x, n, P):
     Returns a 1D numpy.ndarray containing the likelihood of obtaining the data
         x and n for each probability in P, respectively
     """
-    if n <= 0 or type(n) is not int:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
     if type(x) is not int or x < 0:
         raise ValueError("x must be in integer that is greater than or equal\
                          to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray and len(P.shape) != 1:
+    if type(P) is not np.ndarray or P.ndim != 1:
         raise TypeError("P must me a 1D numpy.ndarray")
     if ((P > 0) & (P < 1)).all():
         raise ValueError("All values in P must be in the range [0, 1]")
