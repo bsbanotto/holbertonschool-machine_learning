5b55ea5e16fda9ea3c05d594a21a05344353c615
bsbanotto <ben.sbanotto@gmail.com>
2023-05-14 09:02:33 -0500

    New approach to valid values in P

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -39,6 +39,7 @@ def likelihood(x, n, P):
         raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if ((P <= 0) & (P >= 1)).all():
-        raise ValueError("All values in P must be in the range [0, 1]")
+    for value in P:
+        if value < 0 or value > 1:
+            raise ValueError("All values in P must be in the range [0, 1]")
     return 0
