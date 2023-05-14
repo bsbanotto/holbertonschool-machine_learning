5b90db55ab4b5443cfe3768a7b0d7b22ef332aab
bsbanotto <ben.sbanotto@gmail.com>
2023-05-14 09:41:31 -0500

    Added in missing check for type and shape of Pr

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -41,6 +41,8 @@ def intersection(x, n, P, Pr):
         raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
+    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     for value in P:
         if value < 0 or value > 1:
             raise ValueError("All values in P must be in the range [0, 1]")
