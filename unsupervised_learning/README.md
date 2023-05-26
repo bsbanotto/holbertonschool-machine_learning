75e76ab427bdad7c939ed7c599358fd6a8a60512
bsbanotto <ben.sbanotto@gmail.com>
2023-05-26 10:11:16 -0500

    Adding task 0 and readme

---
+++ unsupervised_learning/README.md
@@ -0,0 +1,1 @@
+README for Unsupervised Learning Projects
---
+++ unsupervised_learning/clustering/0-initialize.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Initialze K-means algorithm
+"""
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    X: numpy.ndarray of shape (n, d) containing the dataset used for clustering
+        n: number of data points
+        d: number of dimensions for each data point
+    k: positive integer containing the number of clusters
+    The cluster centroids should be initialized with a multivariate
+    uniform distribution along each dimension in d
+        The minimum and maximum values for the distribution should be the
+        minimum and maximum values of X along each dimension in d
+    Should use numpy.random.uniform exactly once
+    Returns a numpy.ndarray of shape (k, d) containing the initialized
+        centroids for each cluster, or None on failure
+    """
+    min_val = X.min(axis=0)
+    max_val = X.max(axis=0)
+
+    clusters = np.random.uniform(min_val, max_val, size=(k, X.shape[1]))
+
+    return clusters
