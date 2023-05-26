b041d6f73c9951d6650703f358a7e7706b5a5972
bsbanotto <ben.sbanotto@gmail.com>
2023-05-26 14:23:55 -0500

    Removed try except to see if that is messing up the loop counter

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -19,18 +19,17 @@ def initialize(X, k):
     Returns a numpy.ndarray of shape (k, d) containing the initialized
         centroids for each cluster, or None on failure
     """
-    try:
-        if k <= 0:
-            return None
 
-        min_val = X.min(axis=0)
-        max_val = X.max(axis=0)
-
-        centroids = np.random.uniform(min_val, max_val, size=(k, X.shape[1]))
-
-        return centroids
-    except Exception:
+    if k <= 0:
         return None
+
+    min_val = X.min(axis=0)
+    max_val = X.max(axis=0)
+
+    centroids = np.random.uniform(min_val, max_val, size=(k, X.shape[1]))
+
+    return centroids
+
 
 
 def kmeans(X, k, iterations=1000):
