7035c0bc596e44febd077db48b038002d5cb4f5c
bsbanotto <ben.sbanotto@gmail.com>
2023-05-30 10:16:18 -0500

    Committing edge cases for task 6

---
+++ unsupervised_learning/clustering/6-expectation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Calculates the expectation step in the EM algorithm for a GMM
+"""
+import numpy as np
+pdf = __import__('5-pdf').pdf
+
+
+def expectation(X, pi, m, S):
+    """
+    X: numpy.ndarray of shape (n, d) containing the data set
+    pi: numpy.ndarray of shape (k,) containing the priors for each cluster
+    m: numpy.ndarray of shape (k, d) containing the centroid means for each
+        cluster
+    S: numpy.ndarray of shape (k, d, d) containing the covariance matrices for
+        each cluster
+    Returns g, l or None, None on failure
+        g: numpy.ndarray of shape (k, n) containing the posterior probabilities
+            for each data point in each cluster
+        l: total log likelihood
+    """
+    # Guard agains bad data input
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None
+    if type(pi) is not np.ndarray or len(pi.shape) != 1:
+        return None, None
+    if type(m) is not np.ndarray or len(m.shape) != 2:
+        return None, None
+    if type(S) is not np.ndarray or len(S.shape) != 3:
+        return None, None
+    if X.shape[1] != m.shape[1] or m.shape[1] != S.shape[1]:
+        return None, None
+    if S.shape[1] != S.shape[2]:
+        return None, None
+    if pi.shape[0] != m.shape[0] or m.shape[0] != S.shape[0]:
+        return None, None
