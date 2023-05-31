55e647bd99597c45c818ecd7b69ba66782c9de35
bsbanotto <ben.sbanotto@gmail.com>
2023-05-31 13:16:19 -0500

    Commit for task 7

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -25,6 +25,24 @@ def maximization(X, g):
         return None, None, None
     if X.shape[0] != g.shape[1]:
         return None, None, None
-    if np.sum(g.shape[0] != 0):
-        return None, None, None
-    return("hello")
+    # if np.sum(g.shape[0] != 0):
+    #     return None, None, None
+
+    n, d = X.shape
+    k = g.shape[0]
+
+    pi = np.zeros(k)
+    m = np.zeros((k, d))
+    S = np.zeros((k, d, d))
+
+    # Update the priors of each cluster
+    pi = np.sum(g, axis=1) / n
+
+    # Update the centroid means of each cluster
+    m = g @ X / np.sum(g, axis=1)[:, np.newaxis]
+
+    # Update the covariance matrices of each cluster
+    for i in range(k):
+        S[i] = (g[i] * ((X - m[i]).T)) @ (X - m[i]) / np.sum(g[i])
+
+    return pi, m, S
