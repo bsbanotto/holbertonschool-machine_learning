e96e1065c81790ea24dec02796c270f5e7a6f7a0
bsbanotto <ben.sbanotto@gmail.com>
2023-06-26 12:07:51 -0500

    Adding README and file for task 0

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Create a simple 'vanilla' autoencoder
+"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+    Creates a vanilla autoencoder
+
+    Args:
+        input_dims - integer containing the dimensions of the model input
+        hidden_layers - list containing the number of nodes for each hidden
+            layer in the encoder, respectively
+        latent_dims - integer containing the dimensions of the latent space
+            representation
+
+    Returns:
+        encoder - the encoder model
+        decoder - the decoder model
+        auto - the full autoencoder model
+    """
+    # First, build the encoder
+    input_img = keras.Input(shape=(input_dims,))
+    encoded = input_img
+    for layer in hidden_layers:
+        encoded = keras.layers.Dense(layer, activation='relu')(encoded)
+    encoded = keras.layers.Dense(latent_dims, activation='relu')(encoded)
+    encoder = keras.Model(input_img, encoded, name='encoder')
+
+    # Now, build the decoder
+    decoder_img = keras.Input(shape=(latent_dims))
+    decoded = decoder_img
+    for layer in hidden_layers[::-1]:
+        decoded = keras.layers.Dense(layer, activation='relu')(decoded)
+    decoded = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
+    decoder = keras.Model(decoder_img, decoded, name='decoder')
+
+    # Let's put it all together
+    encoded_output = encoder(input_img)
+    decoded_output = decoder(encoded_output)
+    autoencoder = keras.Model(input_img,
+                              decoded_output,
+                              name='autoencoder')
+
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, autoencoder
