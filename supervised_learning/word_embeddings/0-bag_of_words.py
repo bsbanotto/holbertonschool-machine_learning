1257c95f21a24bf34648aa3501ca748f0677cb60
bsbanotto <ben.sbanotto@gmail.com>
2023-07-28 14:51:01 -0500

    I think documentation was missing in one of my imports"

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -3,8 +3,6 @@
 Bag of Words
 """
 from sklearn.feature_extraction.text import CountVectorizer
-from nltk.stem.porter import PorterStemmer
-from nltk.stem import WordNetLemmatizer
 
 import re
 import string
