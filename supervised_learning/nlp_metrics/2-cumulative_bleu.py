69f1098beb2a87c5020fe76f71f9e0f25ac0538b
bsbanotto <ben.sbanotto@gmail.com>
2023-08-04 11:23:16 -0500

    Commit for task 2 hoping we can import from task 1

--- supervised_learning/nlp_metrics/2-cumulative_bleu.py
+++ supervised_learning/nlp_metrics/2-cumulative_bleu.py
@@ -2,11 +2,26 @@
 """
 Calculates the cumulative n=gram BLEU score of a sentence
 """
-from nltk.translate.bleu_score import sentence_bleu
+import numpy as np
+from scipy.stats import gmean
+
+
+ngram_bleu = __import__('1-ngram_bleu').ngram_bleu
 
 
 def cumulative_bleu(references, sentence, n):
     """
-    Doc here to test checker
+    Args:
+        references: list of reference translations
+            - Each reference translataion is a list of the words in the
+              translation
+        sentence: list containing the model proposed sentence
+        n: size of the largest n-gram to use for evaluation
+
+    Returns:
+        the unigram BLEU score
     """
-    return sentence_bleu(references, sentence)
+    n_gram_scores = []
+    for i in range(1, n + 1):
+        n_gram_scores.append(ngram_bleu(references, sentence, i))
+    return(gmean(n_gram_scores))
