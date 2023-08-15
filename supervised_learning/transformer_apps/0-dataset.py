cbf56344cc2d42ff1fd2a5b02bd731a0fc4e33c6
bsbanotto <ben.sbanotto@gmail.com>
2023-08-15 15:29:51 -0500

    Committing tasks 0 and 1

---
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Transformer Applications Project
+"""
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """
+    Loads and prepares a dataset for machine translation
+    """
+    def __init__(self):
+        """
+        Class constructor for Dataset class
+        """
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train',
+                                    as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation',
+                                    as_supervised=True)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train
+            )
+
+    def tokenize_dataset(self, data):
+        """
+        Creates sub-word tokenizers for the dataset
+        Args:
+            data: tf.data.Dataset whose examples are formatted as a tuple
+                    (pt, en)
+                pt: tf.Tensor containing the Portuguese sentence
+                en: tf.Tensor containing the corresponding English sentence
+        Maximum Vocab size should be set to 2**15
+        Returns:
+            tokenizer_pt: The Portuguese tokenizer
+            tokenizer_en: The English tokenizer
+        """
+        token_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, _ in data),
+            target_vocab_size=2**15
+            )
+        token_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+            (en.numpy() for _, en in data),
+            target_vocab_size=2**15
+            )
+
+        return token_pt, token_en
+
+
+if __name__ == "__main__":
+    import tensorflow as tf
+
+    data = Dataset()
+    for pt, en in data.data_train.take(1):
+        print(pt.numpy().decode('utf-8'))
+        print(en.numpy().decode('utf-8'))
+    for pt, en in data.data_valid.take(1):
+        print(pt.numpy().decode('utf-8'))
+        print(en.numpy().decode('utf-8'))
+    print(type(data.tokenizer_pt))
+    print(type(data.tokenizer_en))
