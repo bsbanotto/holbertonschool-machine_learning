728d7109dc08a5388f9a7286fb8298b9af4d5b7e
bsbanotto <ben.sbanotto@gmail.com>
2023-10-17 11:22:21 -0500

    Refactored some to see if I can get the user not found fixed

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -17,16 +17,16 @@ if __name__ == "__main__":
 
     user_data = response.json()
 
-    if response.status_code == 403:
+    if response.status_code == 200:
+        if user_data['location']:
+            print(user_data['location'])
+        else:
+            print("Not found")
+
+    elif response.status_code == 403:
         rate_url = 'https://api.github.com/rate_limit'
         rate_response = requests.get(rate_url)
         rate_response_data = rate_response.json()
         reset_time = rate_response_data['rate']['reset']
         now = int(time.time())
         print("Reset in {} min".format((reset_time - now) // 60))
-
-    else:
-        if user_data['location']:
-            print(user_data['location'])
-        else:
-            print("Not found")
