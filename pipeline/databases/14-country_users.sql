065316a7975dd95734514f37fab40c84f1d329a1
root <ben.sbanotto@gmail.com>
2023-10-29 19:10:16 +0000

    Commit for task 14

---
+++ pipeline/databases/14-country_users.sql
@@ -0,0 +1,11 @@
+-- Creates a table `users` with the following attributes
+-- id: integer, never NULL, auto increment, is primary key
+-- email: string, 255 characters, never NULL is unique
+-- name: string, 255 characters
+-- country: Enumeration fo US, CO, TN, never NULL, Default is first element
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(256),
+    country ENUMERATION('US', 'CO', 'TN') NOT NULL
+);
