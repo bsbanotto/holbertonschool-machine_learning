eb1f4e38c95bcb412e4c0e5286a96bc1827cce73
root <ben.sbanotto@gmail.com>
2023-10-29 19:14:33 +0000

    Trusted auto-fill, enumerate vs enum

--- pipeline/databases/14-country_users.sql
+++ pipeline/databases/14-country_users.sql
@@ -6,6 +6,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(256),
-    country ENUMERATION('US', 'CO', 'TN') NOT NULL
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
 );
