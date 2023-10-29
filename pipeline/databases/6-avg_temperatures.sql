d98f4851ae87c62e68b10ebdb21b4b79894780d9
root <ben.sbanotto@gmail.com>
2023-10-29 18:06:22 +0000

    Renamed column from average_temp to avg_temp

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Displays the average temperature by city ordered by temperature (descending)
-SELECT city, AVG(value) AS average_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY average_temp DESC;
+ORDER BY avg_temp DESC;
