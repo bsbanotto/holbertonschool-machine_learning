4d790a41214fec83b023e42acf9aad16fe69f1f8
root <ben.sbanotto@gmail.com>
2023-10-30 13:54:37 -0700

    Task 30

---
+++ pipeline/databases/30-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+List all documents in a collection
+"""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """
+    List all documents in a collection
+    """
+    return mongo_collection.find()
