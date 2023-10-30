2c41566e274b8f027110ac15ea1ee594b2ddb0eb
root <ben.sbanotto@gmail.com>
2023-10-30 17:07:56 +0000

    It helps to look at the MySQL 5.7 docs

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -1,8 +1,14 @@
 -- Resets the attribute valid_email only when the email has been changed
+delimiter //
 DROP TRIGGER IF EXISTS valid_email;
 CREATE TRIGGER valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-IF old.email != new.email THEN
-    SET new.valid_email = 0;
-END IF;
+BEGIN
+    IF old.email != new.email THEN
+        SET new.valid_email = 0;
+    ELSE
+        SET new.valid_email = 1;
+    END IF;
+END;//
+delimiter ;
