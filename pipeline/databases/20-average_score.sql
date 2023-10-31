7465178a723c087a780b0bf290c64b7dd9716d5e
root <ben.sbanotto@gmail.com>
2023-10-31 20:46:53 +0000

    Commit task 20

---
+++ pipeline/databases/20-average_score.sql
@@ -0,0 +1,24 @@
+-- SQL script that creates a stored procedure `ComputeAverageScoreForUser`
+-- that computes and stores the average score for a student
+delimiter //
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE users_average_score INT;
+
+    -- Calculate the average score for the user
+    SELECT AVG(score) INTO users_average_score
+    FROM corrections
+    WHERE corrections.user_id = user_id;
+
+    -- Update the user's average score in the users table
+    UPDATE users
+    SET users.average_score = users_average_score
+    WHERE users.id = user_id;
+END;//
+
+delimiter ;
